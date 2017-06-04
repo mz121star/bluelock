9ad345fed41c1718e723b7bcce9373354113ac07
苗壮 <miaozhuang@miaozhuangdemacbook-pro.local>
2017-06-04 23:40:37 +0800

    Initial Commit

---
+++ kaisuo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kaisuo
+//
+//  Created by 苗壮 on 2017/6/4.
+//  Copyright © 2017年 苗壮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kaisuo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kaisuo
+//
+//  Created by 苗壮 on 2017/6/4.
+//  Copyright © 2017年 苗壮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
